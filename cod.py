abc25db9020d5493628bdeeeb1595fd7210d0b65
Hman <haman900825@gmail.com>
2025-05-11 23:42:15 +0330

    .1

---
+++ cod.py
@@ -0,0 +1,45 @@
+import sys
+from PyQt5.QtWidgets import QApplication, QLabel, QWidget, QVBoxLayout
+from PyQt5.QtGui import QPixmap
+
+class Example(QWidget):
+
+    def __init__(self):
+        super().__init__()
+
+        self.initUI()
+
+
+    def initUI(self):
+
+        # ایجاد یک شیء QLabel برای نمایش عکس
+        self.label = QLabel(self)
+
+        # بارگذاری عکس از فایل
+        pixmap = QPixmap('.....')  
+
+        # تنظیم عکس در QLabel
+        self.label.setPixmap(pixmap)
+
+        # تغییر اندازه QLabel به اندازه عکس
+        self.label.resize(pixmap.width(), pixmap.height())
+
+        # ایجاد یک طرح‌بندی عمودی و افزودن QLabel به آن
+        vbox = QVBoxLayout()
+        vbox.addWidget(self.label)
+
+        # تنظیم طرح‌بندی روی پنجره اصلی
+        self.setLayout(vbox)
+
+        # تنظیم عنوان پنجره
+        self.setWindowTitle('نمایش عکس')
+
+        # نمایش پنجره
+        self.show()
+
+
+if __name__ == '__main__':
+
+    app = QApplication(sys.argv)
+    ex = Example()
+    sys.exit(app.exec_())
